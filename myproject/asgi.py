7127d473b7c63cc78f938bd6507efa2ade580453
yohana <yohanakipruto@gmail.com>
2026-09-12 09:29:51 +0300

    made changes on files

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-settings_module = 'auth.settings_deployment' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'myproject.settings'
+settings_module = 'myproject.settings_deployment' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'myproject.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'auth.settings_deployment' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'myproject.settings'
+settings_module = 'myproject.settings_deployment' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'myproject.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
